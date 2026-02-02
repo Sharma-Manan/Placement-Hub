ff11882f5325f50e40787924855e6cc5ab8f4363
manan-25 <msharma3732@gmail.com>
2026-02-02 16:35:04 +0530

    updated

---
+++ backend/app/main.py
@@ -0,0 +1,16 @@
+"""
+main.py
+This is the entry point of the FastAPI application.
+It initializes the app and includes API routers.
+"""
+
+from fastapi import FastAPI
+
+app = FastAPI(
+    title="AI-Based Smart Placement & Internship Coordination System",
+    version="1.0.0" 
+)
+
+@app.get("/")
+def root():
+    return {"status" : "Backend is running"}
