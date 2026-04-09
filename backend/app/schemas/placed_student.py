36b2db48229eed5e3a185f718b13476dce94dd9a
manan-25 <msharma3732@gmail.com>
2026-04-10 00:56:32 +0530

    updated placed

--- backend/app/schemas/placed_student.py
+++ backend/app/schemas/placed_student.py
@@ -16,3 +16,14 @@ class PlacedStudentListOut(BaseModel):
 
     class Config:
         from_attributes = True
+
+class PlacedStudentOut(BaseModel):
+    placed_student_id: UUID
+    student_name: str
+    company_name: str
+    role: str
+    ctc_lpa: Optional[float]
+    already_on_wall: bool
+
+    class Config:
+        orm_mode = True
